d4e16c0b13edd45f55168c3d32dc5df157580b2a
hdriqi <riqi.asdf@gmail.com>
2021-10-07 00:42:17 +0700

    update: finalize API & Styling

--- components/TopCollections.js
+++ components/TopCollections.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import cachios from 'cachios'
 import { useEffect, useState } from 'react'
 import { parseImgUrl, prettyBalance } from 'utils/common'
 
@@ -6,22 +6,27 @@ const HEADERS = [
   {
     id: 'collection_id',
     title: 'Collection',
-    className: `w-3/6`,
+    className: `flex w-4/6 lg:w-2/6 flex-shrink-0 p-3 h-full`,
   },
   {
     id: 'volume_usd',
     title: 'Volume',
-    className: `w-1/6 text-right`,
+    className: `flex items-center w-2/6 lg:w-1/6 justify-end flex-shrink-0 p-3 h-full`,
   },
   {
     id: 'avg_price_usd',
     title: 'Avg. Price',
-    className: `w-1/6 text-right`,
+    className: `flex items-center w-2/6 lg:w-1/6 justify-end flex-shrink-0 p-3 h-full`,
   },
   {
-    id: 'owner_count',
+    id: 'total_owners',
     title: 'Owners',
-    className: `w-1/6 text-right`,
+    className: `flex items-center w-2/6 lg:w-1/6 justify-end flex-shrink-0 p-3 h-full`,
+  },
+  {
+    id: 'total_cards',
+    title: 'Cards',
+    className: `flex items-center w-2/6 lg:w-1/6 justify-end flex-shrink-0 p-3 h-full`,
   },
 ]
 
@@ -36,7 +41,7 @@ const RowCollection = ({ d }) => {
 
   const fetchData = async () => {
     try {
-      const resp = await axios.get(
+      const resp = await cachios.get(
         `https://api-v2-mainnet.paras.id/collections`,
         {
           params: {
@@ -51,17 +56,20 @@ const RowCollection = ({ d }) => {
   }
 
   return (
-    <a href={`https://paras.id/collection/${d.collection_id}`}>
-      <div className="flex items-center p-3 hover:bg-gray-800">
-        <div className={`${HEADERS[0].className} flex items-center h-12`}>
-          <div className="w-10 h-10 bg-blue-900 rounded-full overflow-hidden">
+    <a
+      className="w-full"
+      href={`https://paras.id/collection/${d.collection_id}`}
+    >
+      <div className="flex items-center h-16 lg:hover:bg-gray-800">
+        <div className={`${HEADERS[0].className} flex items-center`}>
+          <div className="flex-shrink-0 flex-grow-1 w-8 md:w-10 h-8 md:h-10 bg-blue-900 rounded-full overflow-hidden">
             {collection?.media && (
               <img src={parseImgUrl(collection?.media)} className="bg-cover" />
             )}
           </div>
-          <div className="pl-4">
-            <p className="font-semibold">{collection?.collection}</p>
-            <span className="text-sm text-gray-300">
+          <div className="pl-4 overflow-hidden">
+            <p className="font-semibold truncate">{collection?.collection}</p>
+            <span className="text-sm text-gray-300 truncate">
               by{` `}
               <a
                 href={`https://paras.id/${collection?.creator_id}/creation`}
@@ -78,7 +86,8 @@ const RowCollection = ({ d }) => {
         <div className={HEADERS[2].className}>
           ${prettyBalance(d.avg_price_usd, 0, 4)}
         </div>
-        <div className={HEADERS[3].className}>{d.owner_count}</div>
+        <div className={HEADERS[3].className}>{d.total_owners}</div>
+        <div className={HEADERS[4].className}>{d.total_cards}</div>
       </div>
     </a>
   )
@@ -103,43 +112,47 @@ const TopCollections = ({ data }) => {
   }, [sortBy, orderBy])
 
   return (
-    <div className="w-full bg-black bg-opacity-25 rounded-lg overflow-hidden">
-      <div className="flex text-gray-300 border-gray-800 border-b-2 p-3">
-        {HEADERS.map((d) => {
+    <div className="">
+      <div className="overflow-x-auto bg-black bg-opacity-25 w-full rounded-lg">
+        <div className="">
+          <div className="flex text-gray-300 ">
+            {HEADERS.map((d) => {
+              return (
+                <div
+                  className={`${d.className} cursor-pointer hover:opacity-75 border-gray-800 border-b-2`}
+                  onClick={() => {
+                    if (sortBy === d.id) {
+                      if (orderBy === 'desc') {
+                        setOrderBy('asc')
+                      } else {
+                        setOrderBy('desc')
+                      }
+                    } else {
+                      setSortBy(d.id)
+                      setOrderBy('desc')
+                    }
+                  }}
+                >
+                  <span>{d.title}</span>
+                  {sortBy === d.id ? (
+                    <span className="inline-block pl-2">
+                      {orderBy === 'desc' ? '↓' : '↑'}
+                    </span>
+                  ) : null}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        {localData.slice(page * LIMIT, (page + 1) * LIMIT).map((d) => {
           return (
-            <div
-              className={`${d.className} cursor-pointer hover:opacity-75`}
-              onClick={() => {
-                if (sortBy === d.id) {
-                  if (orderBy === 'desc') {
-                    setOrderBy('asc')
-                  } else {
-                    setOrderBy('desc')
-                  }
-                } else {
-                  setSortBy(d.id)
-                  setOrderBy('desc')
-                }
-              }}
-            >
-              <span>{d.title}</span>
-              {sortBy === d.id ? (
-                <span className="inline-block pl-2">
-                  {orderBy === 'desc' ? '↓' : '↑'}
-                </span>
-              ) : null}
+            <div key={d.collection_id}>
+              <RowCollection d={d} />
             </div>
           )
         })}
       </div>
-      {localData.slice(page * LIMIT, (page + 1) * LIMIT).map((d) => {
-        return (
-          <div key={d.collection_id}>
-            <RowCollection d={d} />
-          </div>
-        )
-      })}
-      <div className="flex items-center justify-center p-3">
+      <div className="mt-4 flex items-center justify-center">
         <button
           className="disabled:opacity-75"
           disabled={page === 0}
@@ -154,7 +167,7 @@ const TopCollections = ({ data }) => {
         </div>
         <button
           className="disabled:opacity-75"
-          disabled={page === Math.ceil(localData.length / LIMIT)}
+          disabled={page + 1 === Math.ceil(localData.length / LIMIT)}
           onClick={() => {
             setPage(page + 1)
           }}
